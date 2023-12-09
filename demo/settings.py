4faff96e3efe81e8771d287786d73d80924b1edf
JJH529529 <126366914+JJH529529@users.noreply.github.com>
2023-12-09 14:12:57 +0800

    增加了mysqlclient的包

--- demo/settings.py
+++ demo/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = "demo.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'spider',
+        'USER': 'root',
+        'PASSWORD':'jh529529',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
